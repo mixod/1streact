9644ee06552620879f593203377aa00d6beb31d5
Sangram Thapa <thapasangram64@gmail.com>
2024-12-23 19:13:53 +0545

    calc

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,59 +1,67 @@
-import { useState } from "react"
-
+import { useState } from "react";
+import './index.css'
 
 function Calculator() {
-    const [state, setState] = useState(" ");
+    const [state, setState]=useState(" ");
     const [value, setValue]=useState(" ");
-    const [value2, setValue2] = useState(" ");
-
+    const [value1, setValue1]=useState(" ");
+   
+   
     function add(){
-        const result= value + value2;
+        const result=value + value1;
         setState(result);
     }
     function sub(){
-        const result= value - value2;
+        const result=value - value1;
         setState(result);
     }
     function mul(){
-        const result= value * value2;
+        const result=value * value1;
         setState(result);
     }
-    function division(){
-        const result= value / value2;
+    function div(){
+        const result=value / value1;
         setState(result);
     }
+
+
+
     function firstNumber(a){
-        const firstnum= parseInt(a.target.value);
-        setValue(firstnum);
+        const numFirst=parseInt(a.target.value);
+        setValue(numFirst);
     }
     function secondNumber(a){
-        const secondnum= parseInt(a.target.value);
-        setValue2(secondnum);
+        const numSecond=parseInt(a.target.value);
+        setValue1(numSecond);
     }
-    function reset() {
+    function reset(){
         setState(" ");
         setValue(" ");
-        setValue2(" ");
+        setValue1(" ");
     }
 
 
-  return (  
-    <>
-    <div className="flex">
+
+
+  return (
+
+    <div className="main">
+    <div className="resultDisplay">: {state}</div>
+    <div className="input">
     <input type="number" value={value} onChange={firstNumber}/>
-    <input type="number" value={value2} onChange={secondNumber}/>
+    <input type="number" value={value1} onChange={secondNumber}/>
     </div>
-    <div className="flex">
+    <div className="operationFlex">
     <button onClick={add}>ADD</button>
     <button onClick={sub}>SUB</button>
     </div>
-    <div className="flex">
+    <div className="operationFlex">
     <button onClick={mul}>MUL</button>
-    <button onClick={division}>Div</button>
+    <button onClick={div}>DIV</button>
     </div>
-    <button onClick={reset}>AC</button>
-    <div className="result">RESULT:{state}</div>
-    </>
+    <button onClick={reset} >AC</button>
+    
+    </div>
   )
 }
 
